e7c9567aeee8132d6795a6d21c651b52b46c6ea7
David Ward <david.a.ward@duke.edu>
2020-05-27 10:31:33 -0400

    Updated testing for Travis

--- analysis.py
+++ analysis.py
@@ -3,14 +3,9 @@
 import calculator as calc
 
 
-
 a = 56
 b = 8
 calc.divide(a, b)
 print(answer)
-calc.add(a,b)
-calc.subtract(a,b)
-
-
-
-
+calc.add(a, b)
+calc.subtract(a, b)
--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 # calculator.py
 
+
 def calc_input():
     a = input("Enter the first number: ")
     b = input("Enter the second number: ")
@@ -8,21 +9,26 @@ def calc_input():
     print("The numbers you entered are {} and {}".format(a, b))
     return a, b
 
+
 def add(a, b):
     print("Add")
     answer = a + b
     print("{} + {} = {}".format(a, b, answer))
+    return answer
+
 
 def subtract(a, b):
     print("Subtract")
     answer = a - b
     print("{} - {} = {}".format(a, b, answer))
 
+
 def multiply(a, b):
     print("Multiply")
     answer = a * b
     print("{} * {} = {}".format(a, b, answer))
-    
+
+
 def divide(a, b):
     print("Name of calculator is {}".format(__name__))
     print("Divide")
@@ -30,10 +36,11 @@ def divide(a, b):
     print("{} * {} = {}".format(a, b, answer))
     return answer
 
+
 def math_command(a, b):
     c = input("Enter a command: ")
     if c == "a":
-        add(a, b)    
+        add(a, b)
     elif c == "s":
         subtract(a, b)
     elif c == "m":
@@ -42,10 +49,10 @@ def math_command(a, b):
         divide(a, b)
     else:
         print("{} is not a valid command".format(c))
-        
+
+
 if __name__ == "__main__":
     x, y = calc_input()
     math_command(x, y)
 
     print("Finished")
-
